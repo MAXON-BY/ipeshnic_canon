07aa5c48e3e1fc33058f4f67faf9f857f9deb53f
MAXON-BY <maxon.pro2015@gmail.com>
2015-06-11 20:23:48 +0300

    canon max

---
+++ ipeshnik/js/agency.js
@@ -0,0 +1,60 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme ()
+ * Code licensed under the Apache License v2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+
+// owl Carousel
+$(document).ready(function() {
+
+    var owl = $("#owl-demo");
+
+    owl.owlCarousel({
+        items : 6, //10 items above 1000px browser width
+        itemsDesktop : [1000,3], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.prev');
+    })
+    $(".play").click(function(){
+        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+    })
+    $(".stop").click(function(){
+        owl.trigger('owl.stop');
+    })
+
+});
+
+// magnific popup
+$('.popup').magnificPopup({type:'image'});
+$('.popup_c').magnificPopup();
